1c6ae8eb113265d4ece5f7b82e591fa47d9d2964
alpharou9 <nakkach.farouk@esprit.tn>
2026-04-23 14:08:43 +0100

    integrate reCAPTCHA v2 on login and registration forms

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,15 +12,23 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
+    public function login(
+        Request             $request,
+        AuthenticationUtils $authenticationUtils,
+        #[Autowire('%recaptcha_site_key%')]
+        string              $siteKey,
+    ): Response {
         if ($this->getUser()) {
             return $this->redirectToRoute('dashboard');
         }
 
+        $error = $authenticationUtils->getLastAuthenticationError();
+
         return $this->render('security/login.html.twig', [
-            'last_username' => $authenticationUtils->getLastUsername(),
-            'error'         => $authenticationUtils->getLastAuthenticationError(),
+            'last_username'   => $authenticationUtils->getLastUsername(),
+            'error'           => $error,
+            'recaptcha_error' => null,
+            'recaptcha_key'   => $siteKey,
         ]);
     }
 
